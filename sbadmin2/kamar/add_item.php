742ec2d5b027295ab79c3a4dd4257997afdaae6c
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2021-12-22 17:19:24 +0700

    Add sbadmin v2

---
+++ sbadmin2/kamar/add_item.php
@@ -0,0 +1,40 @@
+<div class="tab-pane fade" id="add-item" role=" tabpanel" aria-labelledby="profile-tab">
+    <div class="card shadow mb-4 mt-2">
+        <div class=" card-body">
+            <form action="/admin/item" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="tipe">Tipe Kamar</label>
+                    <input type="text" class="form-control" id="tipe" name="tipe" placeholder="Masukkan tipe Kamar" required>
+                </div>
+                <div class="form-group">
+                    <label for="harga">Harga Kamar</label>
+                    <input type="number" class="form-control" id="harga" name="harga" placeholder="Masukkan Harga" required>
+                </div>
+                <div class="form-group">
+                    <label for="jumlah">Jumlah Kamar Tersedia</label>
+                    <input type="number" class="form-control" id="jumlah" name="jumlah" placeholder="Masukkan Jumlah Kamar" required>
+                </div>
+                <div class="form-group">
+                    <label>Nama Hotel</label>
+                    <select name="categoryId" id="categoryId" class="form-control">
+                        <option value="">--- Hotel ---</option>
+
+                        <option value="">Dummy</option>
+
+
+                    </select>
+                </div>
+                <div class=" form-group">
+                    <label for="image">Image</label>
+                    <input type="file" class="form-control" name="image" multiple required>
+                </div>
+                <div class="form-group">
+                    <label for="deskripsi">Deskripsi Kamar</label>
+                    <textarea class="form-control" id="deskripsi" name="deskripsi" required placeholder="Masukkan Deskripsi Kamar"></textarea>
+
+                </div>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
+    </div>
+</div>
---
+++ sbadmin2/kamar/tables.php
@@ -0,0 +1,38 @@
+<div class="tab-pane fade show active" id="show-item" role="tabpanel" aria-labelledby="home-tab">
+  <div class="card shadow mb-4 mt-2">
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Tipe</th>
+              <th>Harga</th>
+              <th>Jumlah</th>
+              <th>Deskripsi</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Dummy</td>
+              <td>Dummy</td>
+              <td>Dummy</td>
+              <td>Dummy</td>
+              <td>Dummy</td>
+              <td>
+                <form action="#" method=" POST">
+                  <a href="#" class="btn btn-info btn-circle btn-sm"><i class="fas fa-image"></i></a>
+                  <a href="#" class="btn btn-warning btn-circle btn-sm "><i class="fas fa-edit"></i></a>
+                  <a href="#" class="btn btn-success btn-circle btn-sm"><i class="fas fa-plus"></i></a>
+                  <button type="submit" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i>
+                  </button>
+                </form>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>

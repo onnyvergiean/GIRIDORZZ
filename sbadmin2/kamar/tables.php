8af76486558689262a078344b40fd49e6b6bb5c3
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2021-12-22 18:14:01 +0700

    Add Method Read for data Kamar

--- sbadmin2/kamar/tables.php
+++ sbadmin2/kamar/tables.php
@@ -1,3 +1,12 @@
+<?php
+require '../db_conn.php';
+
+$result = mysqli_query($conn, "SELECT * FROM kamar ORDER BY idKamar") or die(mysqli_error($conn));
+
+while ($data = mysqli_fetch_array($result)) {
+  $rows[] = $data;
+}
+?>
 <div class="tab-pane fade show active" id="show-item" role="tabpanel" aria-labelledby="home-tab">
   <div class="card shadow mb-4 mt-2">
     <div class="card-body">
@@ -14,22 +23,24 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Dummy</td>
-              <td>Dummy</td>
-              <td>Dummy</td>
-              <td>Dummy</td>
-              <td>Dummy</td>
-              <td>
-                <form action="#" method=" POST">
-                  <a href="#" class="btn btn-info btn-circle btn-sm"><i class="fas fa-image"></i></a>
-                  <a href="#" class="btn btn-warning btn-circle btn-sm "><i class="fas fa-edit"></i></a>
-                  <a href="#" class="btn btn-success btn-circle btn-sm"><i class="fas fa-plus"></i></a>
-                  <button type="submit" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i>
-                  </button>
-                </form>
-              </td>
-            </tr>
+            <?php foreach ($rows as $row) : ?>
+              <tr>
+                <td><?= $row['idKamar'] ?></td>
+                <td><?= $row['tipeKamar'] ?></td>
+                <td><?= $row['hargaKamar'] ?></td>
+                <td><?= $row['jumlahKamar'] ?></td>
+                <td><?= $row['deskripsiKamar'] ?></td>
+                <td>
+                  <form action="#" method=" POST">
+                    <a href="#" class="btn btn-info btn-circle btn-sm"><i class="fas fa-image"></i></a>
+                    <a href="#" class="btn btn-warning btn-circle btn-sm "><i class="fas fa-edit"></i></a>
+                    <a href="#" class="btn btn-success btn-circle btn-sm"><i class="fas fa-plus"></i></a>
+                    <button type="submit" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i>
+                    </button>
+                  </form>
+                </td>
+              </tr>
+            <?php endforeach; ?>
           </tbody>
         </table>
       </div>

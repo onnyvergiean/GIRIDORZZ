d315897a6b53268dc25dfc63f8671652a6ea1205
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2022-01-10 23:22:43 +0700

    Hapus

--- index.php
+++ index.php
@@ -1,233 +1,200 @@
 <?php
 require_once('header.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<section class="container">
+  <div class="card mb-3 border-0 text-center">
+    <img src="Assets/Images/image 1.png" class="card-img-top" alt="image" />
+    <div class="card-body">
+      <h1 class="card-title fw-bold text-center">Booking hotel, ga pake ribet!</h1>
+      <a href="daftar-hotel.php" class="btn btn-warning btn-lg my-4">Get Started</a>
+    </div>
+  </div>
+</section>
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="shortcut icon" href="./Assets/Images/logo.png" type="image/x-icon">
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="Assets/css/details.css" />
-  <title>Home</title>
-</head>
-
-<body>
-  <header>
-    <nav class="navbar navbar-expand-lg navbar-light container">
-      <a href="index.php" class="brand-text-wrapper">
-        <span class="brand-text-icon">GIRIDORZZ</span>
-      </a>
-      <nav class="navbar-nav navbar-center">
-        <a class="nav-link active"> Home </a>
-        <a class="nav-link" href="history.php"> Booking </a>
-        <a class="nav-link" href="daftar-hotel.php"> Hotels </a>
-      </nav>
-      <nav class="navbar navbar-nav" style="position: absolute; top: 0; right: 0">
-        <a class="nav-link" href="login.php"> Login </a>
-        <form action="register.php">
-          <button class="btn btn-warning nav-link">Sign Up</button>
-        </form>
-      </nav>
-    </nav>
-  </header>
-  <section class="container">
-    <div class="card mb-3 border-0 text-center">
-      <img src="Assets/Images/image 1.png" class="card-img-top" alt="image" />
-      <div class="card-body">
-        <h1 class="card-title fw-bold text-center">Booking hotel, ga pake ribet!</h1>
-        <a href="daftar-hotel.php" class="btn btn-warning btn-lg my-4">Get Started</a>
+<main class="container" style="margin-top: 100px;">
+  <div class="card" style="background-color: #f5f5f5; height: 500px; border-radius: 30px; border: 0cm;">
+    <h1 class="about-title" style="margin-top: 42px; font-size: 36px; font-weight: bold; margin-left: 30px">
+      Rekomendasi Hotel Untukmu</h1>
+    <div class="row" style="margin-top: 40px">
+      <div class="col-3" style=" margin-left: 10px;">
+        <div class=" card-home" style="width: 320px; ">
+          <div class="rating">4.5</div>
+          <div class="price">Rp. 200.000/malam</div>
+          <img style="height: 300px;" src="Assets/Images/Rectangle 4.png" alt="" />
+          <div class="layer-shadow">
+            <h5>Ocean Land</h5>
+            <h7>Bandung, Indonesia</h7>
+          </div>
+        </div>
+      </div>
+      <div class="col-3" style="  margin-left: 70px;">
+        <div class="card-home" style="width: 320px; ">
+          <div class="rating">4.5</div>
+          <div class="price">Rp. 200.000/malam</div>
+          <img style="height: 300px;" src="Assets/Images/Rectangle 3.png" alt="" />
+          <div class="layer-shadow">
+            <h5>Ocean Land</h5>
+            <h7>Bandung, Indonesia</h7>
+          </div>
+        </div>
+      </div>
+      <div class="col-3" style=" margin-left: 70px;">
+        <div class="card-home" style="width: 320px; ">
+          <div class="rating">4.5</div>
+          <div class="price">Rp. 200.000/malam</div>
+          <img style="height: 300px;" src="Assets/Images/Rectangle 5.png" alt="" />
+          <div class="layer-shadow">
+            <h5>Ocean Land</h5>
+            <h7>Bandung, Indonesia</h7>
+          </div>
+        </div>
       </div>
     </div>
-  </section>
-
-  <main class="container" style="margin-top: 100px;">
-    <div class="card" style="background-color: #f5f5f5; height: 500px; border-radius: 30px; border: 0cm;">
-      <h1 class="about-title" style="margin-top: 42px; font-size: 36px; font-weight: bold; margin-left: 30px">
-        Rekomendasi Hotel Untukmu</h1>
-      <div class="row" style="margin-top: 40px">
-        <div class="col-3" style=" margin-left: 10px;">
-          <div class=" card-home" style="width: 320px; ">
-            <div class="rating">4.5</div>
-            <div class="price">Rp. 200.000/malam</div>
-            <img style="height: 300px;" src="Assets/Images/Rectangle 4.png" alt="" />
-            <div class="layer-shadow">
-              <h5>Ocean Land</h5>
-              <h7>Bandung, Indonesia</h7>
-            </div>
-          </div>
-        </div>
-        <div class="col-3" style="  margin-left: 70px;">
-          <div class="card-home" style="width: 320px; ">
-            <div class="rating">4.5</div>
-            <div class="price">Rp. 200.000/malam</div>
-            <img style="height: 300px;" src="Assets/Images/Rectangle 3.png" alt="" />
-            <div class="layer-shadow">
-              <h5>Ocean Land</h5>
-              <h7>Bandung, Indonesia</h7>
-            </div>
-          </div>
-        </div>
-        <div class="col-3" style=" margin-left: 70px;">
-          <div class="card-home" style="width: 320px; ">
-            <div class="rating">4.5</div>
-            <div class="price">Rp. 200.000/malam</div>
-            <img style="height: 300px;" src="Assets/Images/Rectangle 5.png" alt="" />
-            <div class="layer-shadow">
-              <h5>Ocean Land</h5>
-              <h7>Bandung, Indonesia</h7>
-            </div>
-          </div>
-        </div>
-      </div>
-      <a href="daftar-hotel.php" class="btn btn-warning btn-lg my-5" style="  width: 210px;
+    <a href="daftar-hotel.php" class="btn btn-warning btn-lg my-5" style="  width: 210px;
         border-radius: 12px;
         margin-left: 445px">Lihat Semua Hotel</a>
-    </div>
-  </main>
-
-  <main class="container news">
-    <div class="h1 fw-bold" style="margin-top: 90px;">News</div>
-    <div class="card mb-3 align-content-center" style="max-width: 800px;  margin-top: 40px; border: 0cm;">
-      <div class="row no-gutters">
-        <div class="col-md-8">
-          <div class=" card-body">
-            <h1 class="card-title fw-bold">Hotel's Latest Promo</h1>
-            <p class="card-text text-muted">We provide what you need, with a new atmosphere and
-              year-end promos making
-              unforgettable moment of your
-              vacation.</p>
-            <del>
-              <h3 class="card-text text-danger my-1">Rp. 100.000 / Malam</h3>
-            </del>
-            <h3 class="card-text text-primary my-2" style="text-indent: 30px;">Rp. 60.000 / Malam</h3>
-            <a href="detail-kamar.php" class="btn btn-lg btn-warning shadow-sm my-4" style="margin-left: 90px;">Check
-              Now</a>
-          </div>
-        </div>
-        <div class="col-4 ">
-          <div class="card-news" style="width:600px; height: 370px; border-top-left-radius: 100px;">
-            <div class="diskon">40%</div>
-            <div class="price">Rp. 200.000/malam</div>
-            <img style="height: 400px;" src="Assets/Images/Rectangle 3.png" alt="" />
-            <div class="layer-shadow">
-              <h5>Ocean Land</h5>
-              <h7>Bandung, Indonesia</h7>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
-    </div>
-  </main>
-
-  <div class="container" style="margin-top: 100px;">
-    <div class="card" style="background-color: #f5f5f5; height: 500px;  border-radius: 30px; ">
-      <h1 class="about-title" style="margin-top: 42px; font-size: 36px; font-weight: bold; margin-left: 30px">Giridorzz
-        Promos</h1>
-      <div class="row" style="margin-top: 40px">
-        <div class="col-3">
-          <div class="card-home" style="width: 320px;">
-            <div class="rating">40%</div>
-            <div class="price">Rp. 200.000/malam</div>
-            <img style="height: 320px;" src="Assets/Images/Rectangle 4.png" alt="" />
-            <div class="layer-shadow">
-              <h5>Ocean Land</h5>
-              <h7>Bandung, Indonesia</h7>
-            </div>
-          </div>
-        </div>
-        <div class="col-3">
-          <div class="card-home" style="width: 320px; margin-left: 108px">
-            <div class="rating">15%</div>
-            <div class="price">Rp. 200.000/malam</div>
-            <img style="height: 320px;" src="Assets/Images/Rectangle 3.png" alt="" />
-            <div class="layer-shadow">
-              <h5>Ocean Land</h5>
-              <h7>Bandung, Indonesia</h7>
-            </div>
-          </div>
-        </div>
-        <div class="col-3">
-          <div class="card-home" style="width: 320px; margin-left: 190px">
-            <div class="rating">20%</div>
-            <div class="price">Rp. 200.000/malam</div>
-            <img style="height: 320px;" src="Assets/Images/Rectangle 5.png" alt="" />
-            <div class="layer-shadow">
-              <h5>Ocean Land</h5>
-              <h7>Bandung, Indonesia</h7>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row my-3">
-        <div class="col">
-          <p class="card-text text-muted text-center" style="margin-left: 65px; margin-right: 40px;">with a 30% promo
-            you can take
-            a vacation without thinking
-            about extra costs</p>
-        </div>
-        <div class="col">
-          <p class="card-text text-muted text-center" style="margin-left: 30px; margin-right: 40px;">with a 40% promo
-            you can take
-            a vacation without thinking
-            about extra costs</p>
-        </div>
-        <div class="col">
-          <p class="card-text text-muted text-center" style="margin-left: 10px; margin-right: 50px;">with a 40% promo
-            you can take
-            a vacation without thinking
-            about extra costs</p>
-        </div>
-      </div>
-    </div>
-    </main>
-
-
-    <footer class="container">
-      <div class="row" style="margin-top: 100px;">
-        <div class="col-3"> <span class="brand-text-icon">GIRIDORZZ</span>
-          <p class="p-footer">We provide what you need to enjoy your
-            holiday with family.</p>
-        </div>
-        <div class="col-4">
-          <span class="footer-title">About <p class="p-footer">GIRIDORZZ akan membantu menghubungkan jutaan
-              travellers dengan
-              berbagai tempat menginap yang luar biasa dan terjamin </p></span>
-        </div>
-        <div class="col-3">
-          <span class="footer-title">Contact Info <div style="margin-top: 12px;"><img class="footer-icon" src=" ./Assets/Images/Icon/ic_round-where-to-vote.png" alt="location">
-              <span class="p-footer" style="margin-top: 12px;">
-                GIRIDORZZ, Yogyakarta
-              </span>
-            </div>
-            <div style="margin-top: 12px;"><img class="footer-icon" src="./Assets/Images/Icon/ic_baseline-local-phone.png" alt="phone">
-              <span class="p-footer">
-                +6221 4012 0888</span>
-            </div>
-            <div style="margin-top: 12px;"><img class="footer-icon" src="./Assets/Images/Icon/ic_round-email.png" alt="phone">
-              <span class="p-footer">
-                cs@giridorzz.com</span>
-            </div>
-          </span>
-        </div>
-        <div class="col-2">
-          <span class="footer-title">Follow Us
-            <div style="margin-top: 12px;">
-              <img class="footer-icon" src="./Assets/Images/Icon/uim_instagram-alt.png" alt="ig">
-              <img class="footer-icon" style="margin-left: 12px;" src="./Assets/Images/Icon/fa-brands_twitter-square.png" alt="ig">
-              <img class="footer-icon" style="margin-left: 12px;" src="./Assets/Images/Icon/brandico_facebook-rect.png" alt="ig">
-            </div>
-          </span>
-        </div>
-      </div>
-      <p class="footer-last-text text-center">Copyright 2021 • All rights reserved • GIRIDORZZ</p>
-    </footer>
   </div>
+</main>
+
+<main class="container news">
+  <div class="h1 fw-bold" style="margin-top: 90px;">News</div>
+  <div class="card mb-3 align-content-center" style="max-width: 800px;  margin-top: 40px; border: 0cm;">
+    <div class="row no-gutters">
+      <div class="col-md-8">
+        <div class=" card-body">
+          <h1 class="card-title fw-bold">Hotel's Latest Promo</h1>
+          <p class="card-text text-muted">We provide what you need, with a new atmosphere and
+            year-end promos making
+            unforgettable moment of your
+            vacation.</p>
+          <del>
+            <h3 class="card-text text-danger my-1">Rp. 100.000 / Malam</h3>
+          </del>
+          <h3 class="card-text text-primary my-2" style="text-indent: 30px;">Rp. 60.000 / Malam</h3>
+          <a href="detail-kamar.php" class="btn btn-lg btn-warning shadow-sm my-4" style="margin-left: 90px;">Check
+            Now</a>
+        </div>
+      </div>
+      <div class="col-4 ">
+        <div class="card-news" style="width:600px; height: 370px; border-top-left-radius: 100px;">
+          <div class="diskon">40%</div>
+          <div class="price">Rp. 200.000/malam</div>
+          <img style="height: 400px;" src="Assets/Images/Rectangle 3.png" alt="" />
+          <div class="layer-shadow">
+            <h5>Ocean Land</h5>
+            <h7>Bandung, Indonesia</h7>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+</main>
+
+<div class="container" style="margin-top: 100px;">
+  <div class="card" style="background-color: #f5f5f5; height: 500px;  border-radius: 30px; ">
+    <h1 class="about-title" style="margin-top: 42px; font-size: 36px; font-weight: bold; margin-left: 30px">Giridorzz
+      Promos</h1>
+    <div class="row" style="margin-top: 40px">
+      <div class="col-3">
+        <div class="card-home" style="width: 320px;">
+          <div class="rating">40%</div>
+          <div class="price">Rp. 200.000/malam</div>
+          <img style="height: 320px;" src="Assets/Images/Rectangle 4.png" alt="" />
+          <div class="layer-shadow">
+            <h5>Ocean Land</h5>
+            <h7>Bandung, Indonesia</h7>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card-home" style="width: 320px; margin-left: 108px">
+          <div class="rating">15%</div>
+          <div class="price">Rp. 200.000/malam</div>
+          <img style="height: 320px;" src="Assets/Images/Rectangle 3.png" alt="" />
+          <div class="layer-shadow">
+            <h5>Ocean Land</h5>
+            <h7>Bandung, Indonesia</h7>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card-home" style="width: 320px; margin-left: 190px">
+          <div class="rating">20%</div>
+          <div class="price">Rp. 200.000/malam</div>
+          <img style="height: 320px;" src="Assets/Images/Rectangle 5.png" alt="" />
+          <div class="layer-shadow">
+            <h5>Ocean Land</h5>
+            <h7>Bandung, Indonesia</h7>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row my-3">
+      <div class="col">
+        <p class="card-text text-muted text-center" style="margin-left: 65px; margin-right: 40px;">with a 30% promo
+          you can take
+          a vacation without thinking
+          about extra costs</p>
+      </div>
+      <div class="col">
+        <p class="card-text text-muted text-center" style="margin-left: 30px; margin-right: 40px;">with a 40% promo
+          you can take
+          a vacation without thinking
+          about extra costs</p>
+      </div>
+      <div class="col">
+        <p class="card-text text-muted text-center" style="margin-left: 10px; margin-right: 50px;">with a 40% promo
+          you can take
+          a vacation without thinking
+          about extra costs</p>
+      </div>
+    </div>
+  </div>
+  </main>
+
+
+  <footer class="container">
+    <div class="row" style="margin-top: 100px;">
+      <div class="col-3"> <span class="brand-text-icon">GIRIDORZZ</span>
+        <p class="p-footer">We provide what you need to enjoy your
+          holiday with family.</p>
+      </div>
+      <div class="col-4">
+        <span class="footer-title">About <p class="p-footer">GIRIDORZZ akan membantu menghubungkan jutaan
+            travellers dengan
+            berbagai tempat menginap yang luar biasa dan terjamin </p></span>
+      </div>
+      <div class="col-3">
+        <span class="footer-title">Contact Info <div style="margin-top: 12px;"><img class="footer-icon" src=" ./Assets/Images/Icon/ic_round-where-to-vote.png" alt="location">
+            <span class="p-footer" style="margin-top: 12px;">
+              GIRIDORZZ, Yogyakarta
+            </span>
+          </div>
+          <div style="margin-top: 12px;"><img class="footer-icon" src="./Assets/Images/Icon/ic_baseline-local-phone.png" alt="phone">
+            <span class="p-footer">
+              +6221 4012 0888</span>
+          </div>
+          <div style="margin-top: 12px;"><img class="footer-icon" src="./Assets/Images/Icon/ic_round-email.png" alt="phone">
+            <span class="p-footer">
+              cs@giridorzz.com</span>
+          </div>
+        </span>
+      </div>
+      <div class="col-2">
+        <span class="footer-title">Follow Us
+          <div style="margin-top: 12px;">
+            <img class="footer-icon" src="./Assets/Images/Icon/uim_instagram-alt.png" alt="ig">
+            <img class="footer-icon" style="margin-left: 12px;" src="./Assets/Images/Icon/fa-brands_twitter-square.png" alt="ig">
+            <img class="footer-icon" style="margin-left: 12px;" src="./Assets/Images/Icon/brandico_facebook-rect.png" alt="ig">
+          </div>
+        </span>
+      </div>
+    </div>
+    <p class="footer-last-text text-center">Copyright 2021 • All rights reserved • GIRIDORZZ</p>
+  </footer>
+</div>
 </body>
 
 </html>

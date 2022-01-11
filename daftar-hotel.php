1a532eefb9434887ec8c6c20410a9c6d15848903
hamcuks <ivannurilham60@gmail.com>
2022-01-11 13:22:29 +0700

    add search

--- daftar-hotel.php
+++ daftar-hotel.php
@@ -5,21 +5,29 @@ $hotel = mysqli_query($conn, "SELECT GROUP_CONCAT(imgurl.imageUrl) as img, hotel
 
 $rows = [];
 
-while($data = mysqli_fetch_array($hotel)) {
+
+if (isset($_POST['search'])) {
+    $keyword = $_POST['keyword'];
+    // die($keyword);
+    $hotel = mysqli_query($conn, "SELECT GROUP_CONCAT(imgurl.imageUrl) as img, hotel.*, (SELECT MIN(kamar.hargaKamar) FROM kamar WHERE kamar.hotelId=hotel.idHotel) as harga FROM imgUrl JOIN hotel ON imgUrl.hotelId=hotel.idHotel WHERE hotel.namaHotel LIKE '%$keyword%' OR hotel.kotaHotel LIKE '%$keyword%'  GROUP BY hotel.idHotel ORDER BY hotel.ratingHotel DESC");
+}
+while ($data = mysqli_fetch_array($hotel)) {
     $rows[] = $data;
 }
 ?>
 <section class="container search-section">
+    <form action="" method="POST">
     <div class="row justify-content-center">
         <div class="col-6">
-            <input type="text" id="" class="form-control search-bar" placeholder="Cari hotel">
+            <input type="text" id="" class="form-control search-bar" placeholder="Cari hotel" name="keyword">
         </div>
         <div class="col-2">
-            <button class="btn btn-warning search-button" href="#">
+            <button class="btn btn-warning search-button" type="submit" name="search">
                 Cari
             </button>
         </div>
     </div>
+    </form>
 </section>
 <main class="container my-5 daftar-hotel">
     <div class="row">
@@ -31,26 +39,32 @@ while($data = mysqli_fetch_array($hotel)) {
         ?>
                 <div class="col-4">
                     <div class="card-hotel">
-                        <div class="rating"><?=$hotel['ratingHotel']?></div>
-                        <div class="price">Rp. <?=(empty($hotel['harga'])) ? "100000" : $hotel['harga']?>/malam</div>
-                        <a href="detail-hotel.php?id=<?=$hotel['idHotel']?>">
-                            <img src="Assets/Images/hotel/<?=$image[0]?>" alt="">
+                        <div class="rating"><?= $hotel['ratingHotel'] ?></div>
+                        <div class="price">Rp. <?= (empty($hotel['harga'])) ? "100000" : $hotel['harga'] ?>/malam</div>
+                        <a href="detail-hotel.php?id=<?= $hotel['idHotel'] ?>">
+                            <img src="Assets/Images/hotel/<?= $image[0] ?>" alt="">
                             <div class="layer-shadow">
-                                <h5><?=$hotel['namaHotel']?></h5>
-                                <h7><?=$hotel['kotaHotel']?></h7>
+                                <h5><?= $hotel['namaHotel'] ?></h5>
+                                <h7><?= $hotel['kotaHotel'] ?></h7>
                             </div>
                         </a>
                     </div>
                 </div>
         <?php
             }
-        }
+        } else {
         ?>
+            <div class="row">
+                <div class="col-12">
+                    <p class="text-center">Pencarian Keyword <?= $keyword?> tidak ditemukan</p>
+                </div>
+            </div>
+        <?php } ?>
 
     </div>
 </main>
 
-<section class="pagination">
+<!-- <section class="pagination">
     <div class="container">
         <div class="row justify-content-center">
             <button class="pagination-control prev">Previous</button>
@@ -60,7 +74,7 @@ while($data = mysqli_fetch_array($hotel)) {
             <button class="pagination-control next">Next</button>
         </div>
     </div>
-</section>
+</section> -->
 
 <?php
 require_once('footer.php');

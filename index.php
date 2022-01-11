5b0b875e220102e4ab563ec87a3ada935e728828
hernanda giri <hernanda.pramudita@students.amikom.ac.id>
2022-01-11 13:34:13 +0700

    add home

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once('header.php');
 ?>
+
 <section class="container">
   <div class="card mb-3 border-0 text-center">
     <img src="Assets/Images/image 1.png" class="card-img-top" alt="image" />
@@ -16,41 +17,32 @@ require_once('header.php');
     <h1 class="about-title" style="margin-top: 42px; font-size: 36px; font-weight: bold; margin-left: 30px">
       Rekomendasi Hotel Untukmu</h1>
     <div class="row" style="margin-top: 40px">
-      <div class="col-3" style=" margin-left: 10px;">
-        <div class=" card-home" style="width: 320px; ">
-          <div class="rating">4.5</div>
-          <div class="price">Rp. 200.000/malam</div>
-          <img style="height: 300px;" src="Assets/Images/Rectangle 4.png" alt="" />
-          <div class="layer-shadow">
-            <h5>Ocean Land</h5>
-            <h7>Bandung, Indonesia</h7>
-          </div>
-        </div>
-      </div>
-      <div class="col-3" style="  margin-left: 70px;">
+      <?php 
+            // $query = "SELECT hotel.idHotel, hotel.namaHotel, hotel.kotaHotel, hotel.deskripsiHotel, hotel.ratingHotel, imgurl.imageId, imgurl.imageUrl FROM hotel INNER JOIN imgurl ON hotel.idHotel=imgurl.hotelId";
+      $query = "SELECT * FROM hotel 
+                INNER JOIN imgurl ON hotel.idHotel=imgurl.hotelId
+                INNER JOIN kamar ON hotel.idHotel=kamar.hotelId
+                LIMIT 3
+                ";
+            $sql = mysqli_query($conn, $query);
+
+            while($data = mysqli_fetch_array($sql)) { ?>
+
+      <div class="col-3" style="  margin-left: 50px;">
         <div class="card-home" style="width: 320px; ">
-          <div class="rating">4.5</div>
-          <div class="price">Rp. 200.000/malam</div>
-          <img style="height: 300px;" src="Assets/Images/Rectangle 3.png" alt="" />
+          <div class="rating"><?= $data['ratingHotel']; ?></div>
+          <div class="price"><?= $data['hargaKamar']; ?>/malam</div>
+          <?= '<img style="height: 300px" src="Assets/Images/hotel/'. $data['imageUrl'] .'">'?>
           <div class="layer-shadow">
-            <h5>Ocean Land</h5>
-            <h7>Bandung, Indonesia</h7>
+            <h5><?= $data['namaHotel'] ?></h5>
+            <h7><?= $data['kotaHotel'] ?></h7>
           </div>
         </div>
       </div>
-      <div class="col-3" style=" margin-left: 70px;">
-        <div class="card-home" style="width: 320px; ">
-          <div class="rating">4.5</div>
-          <div class="price">Rp. 200.000/malam</div>
-          <img style="height: 300px;" src="Assets/Images/Rectangle 5.png" alt="" />
-          <div class="layer-shadow">
-            <h5>Ocean Land</h5>
-            <h7>Bandung, Indonesia</h7>
-          </div>
-        </div>
-      </div>
+<?php } ?>
+
     </div>
-    <a href="daftar-hotel.php" class="btn btn-warning btn-lg my-5" style="  width: 210px;
+    <a href="detail-hotel.php" class="btn btn-warning btn-lg my-5" style="  width: 210px;
         border-radius: 12px;
         margin-left: 445px">Lihat Semua Hotel</a>
   </div>
@@ -62,77 +54,124 @@ require_once('header.php');
     <div class="row no-gutters">
       <div class="col-md-8">
         <div class=" card-body">
-          <h1 class="card-title fw-bold">Hotel's Latest Promo</h1>
-          <p class="card-text text-muted">We provide what you need, with a new atmosphere and
-            year-end promos making
-            unforgettable moment of your
-            vacation.</p>
+          <?php 
+            $queryNewsDiskon = "SELECT MAX(hargaKamar) as hargaMax, max(jmlhDiskon) as diskonMax, namaDiskon, deskripsiDiskon FROM kamar
+                      INNER JOIN diskon ON kamar.idKamar=diskon.idKamar LIMIT 1
+            ";
+            $sqlNewsDiskon = mysqli_query($conn, $queryNewsDiskon);
+
+            while($data_news_diskon = mysqli_fetch_array($sqlNewsDiskon)) { ?>
+
+
+          <h1 class="card-title fw-bold"><?= $data_news_diskon['namaDiskon'] ?></h1>
+          <p class="card-text text-muted"><?= $data_news_diskon['deskripsiDiskon'] ?></p>
           <del>
-            <h3 class="card-text text-danger my-1">Rp. 100.000 / Malam</h3>
+            <h3 class="card-text text-danger my-1">Rp <?= $data_news_diskon['hargaMax'] ?>/malam</h3>
           </del>
-          <h3 class="card-text text-primary my-2" style="text-indent: 30px;">Rp. 60.000 / Malam</h3>
+          <h3 class="card-text text-primary my-2" style="text-indent: 30px;">
+            <?php 
+
+              $hargaMax = (int)$data_news_diskon['hargaMax'];
+
+              $diskonMax = (int)$data_news_diskon['diskonMax'];
+              
+              echo "Rp ";
+              echo $hargaMax - $diskonMax;
+              echo " / malam";
+
+
+             ?>
+
+          </h3>
           <a href="detail-kamar.php" class="btn btn-lg btn-warning shadow-sm my-4" style="margin-left: 90px;">Check
             Now</a>
+
+          <?php } ?>
+
         </div>
       </div>
+
+      <?php 
+        $queryNewsImg = "SELECT * FROM hotel 
+                INNER JOIN imgurl ON hotel.idHotel=imgurl.hotelId
+                INNER JOIN kamar ON hotel.idHotel=kamar.hotelId
+                LIMIT 1
+                ";
+        $queryFindDiskon = "SELECT min(jmlhDiskon) as diskonMax, hargaKamar  FROM kamar INNER JOIN diskon ON kamar.idKamar=diskon.idKamar
+        ";
+        $sqlFindDiskon = mysqli_query($conn, $queryFindDiskon);
+        $sqlNewsImg = mysqli_query($conn, $queryNewsImg);
+        while ($dataFindDiskon = mysqli_fetch_array($sqlFindDiskon)) {
+        
+
+        while($dataNewsImg = mysqli_fetch_array($sqlNewsImg)) { ?>
+
+       
       <div class="col-4 ">
-        <div class="card-news" style="width:600px; height: 370px; border-top-left-radius: 100px;">
-          <div class="diskon">40%</div>
-          <div class="price">Rp. 200.000/malam</div>
-          <img style="height: 400px;" src="Assets/Images/Rectangle 3.png" alt="" />
+        <div class=" card-news" style="width:600px; height: 370px; border-top-left-radius: 100px;">
+          <div class="diskon"><?= $dataFindDiskon['diskonMax']; ?>%</div>
+          <div class="price">Rp <?= number_format($dataFindDiskon['hargaKamar']) ?></div>
+          <?= '<img style="height: 400px" src="Assets/Images/hotel/'. $dataNewsImg['imageUrl'] .'">'?>
           <div class="layer-shadow">
-            <h5>Ocean Land</h5>
-            <h7>Bandung, Indonesia</h7>
+            <h5><?= $dataNewsImg['namaHotel'] ?></h5>
+            <h7><?= $dataNewsImg['kotaHotel'] ?></h7>
           </div>
         </div>
       </div>
+
+      <?php
+         }
+        } 
+        ?>
+
+
+
     </div>
   </div>
   </div>
   </div>
+
+
 </main>
 
 <div class="container" style="margin-top: 100px;">
   <div class="card" style="background-color: #f5f5f5; height: 500px;  border-radius: 30px; ">
     <h1 class="about-title" style="margin-top: 42px; font-size: 36px; font-weight: bold; margin-left: 30px">Giridorzz
       Promos</h1>
-    <div class="row" style="margin-top: 40px">
-      <div class="col-3">
+    <div class="row" style="margin-top: 40px; margin-left: 50px;">
+    <?php 
+        $queryNewsImg = "SELECT * FROM hotel 
+                INNER JOIN imgurl ON hotel.idHotel=imgurl.hotelId
+                INNER JOIN kamar ON hotel.idHotel=kamar.hotelId
+                LIMIT 3
+                ";
+        $queryFindDiskon = "SELECT min(jmlhDiskon) as diskonMax, hargaKamar  FROM kamar INNER JOIN diskon ON kamar.idKamar=diskon.idKamar
+        ";
+        $sqlFindDiskon = mysqli_query($conn, $queryFindDiskon);
+        $sqlNewsImg = mysqli_query($conn, $queryNewsImg);
+        while ($dataFindDiskon = mysqli_fetch_array($sqlFindDiskon)) {
+        
+
+        while($dataNewsImg = mysqli_fetch_array($sqlNewsImg)) { ?>
+
+      <div class="col-3" style="margin: auto; margin-left:1px; margin-right:60px">
         <div class="card-home" style="width: 320px;">
-          <div class="rating">40%</div>
-          <div class="price">Rp. 200.000/malam</div>
-          <img style="height: 320px;" src="Assets/Images/Rectangle 4.png" alt="" />
+          <div class="rating"><?= $dataFindDiskon['diskonMax']; ?>%</div>
+          <div class="price">Rp <?= number_format($dataFindDiskon['hargaKamar']) ?></div>
+          <?= '<img style="height: 300px" src="Assets/Images/hotel/'. $dataNewsImg['imageUrl'] .'">'?>
           <div class="layer-shadow">
-            <h5>Ocean Land</h5>
-            <h7>Bandung, Indonesia</h7>
+          <h5><?= $dataNewsImg['namaHotel'] ?></h5>
+            <h7><?= $dataNewsImg['kotaHotel'] ?></h7>
           </div>
         </div>
       </div>
-      <div class="col-3">
-        <div class="card-home" style="width: 320px; margin-left: 108px">
-          <div class="rating">15%</div>
-          <div class="price">Rp. 200.000/malam</div>
-          <img style="height: 320px;" src="Assets/Images/Rectangle 3.png" alt="" />
-          <div class="layer-shadow">
-            <h5>Ocean Land</h5>
-            <h7>Bandung, Indonesia</h7>
-          </div>
-        </div>
-      </div>
-      <div class="col-3">
-        <div class="card-home" style="width: 320px; margin-left: 190px">
-          <div class="rating">20%</div>
-          <div class="price">Rp. 200.000/malam</div>
-          <img style="height: 320px;" src="Assets/Images/Rectangle 5.png" alt="" />
-          <div class="layer-shadow">
-            <h5>Ocean Land</h5>
-            <h7>Bandung, Indonesia</h7>
-          </div>
-        </div>
-      </div>
+      <?php
+         }
+        } 
+        ?>
     </div>
     <div class="row my-3">
-      <div class="col">
+      <div class="col ">
         <p class="card-text text-muted text-center" style="margin-left: 65px; margin-right: 40px;">with a 30% promo
           you can take
           a vacation without thinking
@@ -155,46 +194,6 @@ require_once('header.php');
   </main>
 
 
-  <footer class="container">
-    <div class="row" style="margin-top: 100px;">
-      <div class="col-3"> <span class="brand-text-icon">GIRIDORZZ</span>
-        <p class="p-footer">We provide what you need to enjoy your
-          holiday with family.</p>
-      </div>
-      <div class="col-4">
-        <span class="footer-title">About <p class="p-footer">GIRIDORZZ akan membantu menghubungkan jutaan
-            travellers dengan
-            berbagai tempat menginap yang luar biasa dan terjamin </p></span>
-      </div>
-      <div class="col-3">
-        <span class="footer-title">Contact Info <div style="margin-top: 12px;"><img class="footer-icon" src=" ./Assets/Images/Icon/ic_round-where-to-vote.png" alt="location">
-            <span class="p-footer" style="margin-top: 12px;">
-              GIRIDORZZ, Yogyakarta
-            </span>
-          </div>
-          <div style="margin-top: 12px;"><img class="footer-icon" src="./Assets/Images/Icon/ic_baseline-local-phone.png" alt="phone">
-            <span class="p-footer">
-              +6221 4012 0888</span>
-          </div>
-          <div style="margin-top: 12px;"><img class="footer-icon" src="./Assets/Images/Icon/ic_round-email.png" alt="phone">
-            <span class="p-footer">
-              cs@giridorzz.com</span>
-          </div>
-        </span>
-      </div>
-      <div class="col-2">
-        <span class="footer-title">Follow Us
-          <div style="margin-top: 12px;">
-            <img class="footer-icon" src="./Assets/Images/Icon/uim_instagram-alt.png" alt="ig">
-            <img class="footer-icon" style="margin-left: 12px;" src="./Assets/Images/Icon/fa-brands_twitter-square.png" alt="ig">
-            <img class="footer-icon" style="margin-left: 12px;" src="./Assets/Images/Icon/brandico_facebook-rect.png" alt="ig">
-          </div>
-        </span>
-      </div>
-    </div>
-    <p class="footer-last-text text-center">Copyright 2021 • All rights reserved • GIRIDORZZ</p>
-  </footer>
-</div>
-</body>
-
-</html>
+  <?php
+  require_once('footer.php');
+  ?>
